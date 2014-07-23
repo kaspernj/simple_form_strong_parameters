6c18924fcbbd773d944d8878d2b7c36fee66b6b2
kaspernj <k@spernj.org>
2014-07-23 15:24:21 +0200

    Added Shippable and CodeClimate.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 ENV["RAILS_ENV"] ||= 'test'
 require_relative "dummy/config/environment"
 require 'rspec/rails'
